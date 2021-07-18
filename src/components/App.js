a32e121a706227dfe430ad78002c83f76f4e00e1
vinaka <k64.yuta29@gmail.com>
2021-07-18 15:00:00 +0900

    displya events, refactor and implement

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, {useReducer, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reducer from '../reducers'
+import Event from './event'
 
 
 //  1回目
@@ -48,9 +49,25 @@ const addEvent = (e) => {
             <th></th>
           </tr>
         </thead>
-        <thead>
-
-        </thead>
+        <tbody>
+          {/* {
+            state.map((event, index) => {
+              const id = event.id
+              const handleClickDeleteButton =() => {
+                dispatch({type: "DELETE_EVENT", id})
+              }
+              return (
+                <tr key={index}>
+                  <td>{id}</td>
+                  <td>{event.title}</td>
+                  <td>{event.body}</td>
+                  <td><button type = "button" className = "btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
+                </tr>
+              )
+            })
+          } */}
+          { state.map((event, index) => (<Event event={event} dispatch={dispatch}/>)) }
+        </tbody>
 
       </table>
 
